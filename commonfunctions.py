3d96230e98aec04d14837038e199fbf4de8d4314
Jonathan Brown <8877948+CaptainMoustache@users.noreply.github.com>
2019-10-11 11:52:33 -0400

    Inital commit

---
+++ commonfunctions.py
@@ -0,0 +1,225 @@
+import discord
+import json
+import datetime
+from datetime import timedelta
+import time
+import random
+import statsapi
+import re
+import dateutil.parser
+import requests
+
+
+class CommonFunctions():
+	async def get_team(self, searchName, message):
+		teamsReturned = statsapi.lookup_team(searchName)
+		if len(teamsReturned) > 1:
+			teamSelected = await self.prompt_team(message, searchName, teamsReturned)
+		elif len(teamsReturned) == 1:
+			teamSelected = teamsReturned[0]
+		elif len(teamsReturned) == 0:
+			await message.channel.send('Hmmm I couldn\'t find and teams using \'' + searchName + '\'')
+			return
+		return teamSelected
+	
+	async def wait_for_response(self, message, userResponse, waitTime):
+		responseFound = False
+		#Set the start time
+		messageTime = datetime.datetime.utcnow()
+		#Give a buffer of 2 seconds back for play in the system time and discord time
+		messageTime = messageTime - timedelta(seconds=2)
+		
+		#Wait defined time
+		for wait in range(1, waitTime):
+			if responseFound:
+				break;
+		
+			time.sleep(1)
+			#Get the last messages
+			rawMessageList = await message.channel.history(limit=5).flatten()
+			
+			messageList = []
+			
+			#Prune all messages by bots and the original message
+			for messages in rawMessageList:
+				if messages.author.bot == False and messages != message:
+					messageList.append(messages)
+			
+			#if the name hasn't been selected yet
+			if responseFound == False:
+				#loop through the past 2 messages
+				for history in range (0, len(messageList)):
+					#The user who requested the list responded
+					if messageList[history].author == message.author:
+						#check if the message was sent after the list of names
+						if messageList[history].created_at > messageTime:
+							#The user responded with the matching prompt
+							if userResponse.upper() in messageList[history].content.upper():
+								#The matching response was found
+								responseFound = True
+								return True
+		return False
+	
+	async def wait_for_number(self, message, limit, waitTime):
+		responseNumber = -1
+		#Set the start time
+		messageTime = datetime.datetime.utcnow()
+		#Give a buffer of 2 seconds back for play in the system time and discord time
+		messageTime = messageTime - timedelta(seconds=2)
+	
+		#Wait defined time
+		for wait in range(1, waitTime):
+			if responseNumber != -1:
+				break;
+			#Get the last messages
+			rawMessageList = await message.channel.history(limit=5).flatten()
+			
+			messageList = []
+			
+			#Prune all messages by bots and the original message
+			for messages in rawMessageList:
+				if messages.author.bot == False and messages != message:
+					messageList.append(messages)
+			
+			#if the number hasn't been selected yet
+			if responseNumber == -1 and len(messageList) > 0:
+				#loop through the past messages
+				for history in range (0, len(messageList)):
+					#The user who requested the list responded
+					if messageList[history].author == message.author:
+						#check if the message was sent after the list of names
+						if messageList[history].created_at > messageTime:
+							#The user responded with a number
+							if messageList[history].content.isdigit():
+								#The number is valid
+								if int(messageList[history].content) <= limit and int(messageList[history].content) > 0:
+									responseNumber = int(messageList[history].content)
+									return responseNumber
+								else:
+									await message.channel.send('%s is not a valid number, start over' % str(messageList[history].content))
+									return
+							else:
+								await message.channel.send('%s is not a number, start over' % messageList[history].content)
+								return
+			#number has not been selected, wait
+			time.sleep(1)
+		#if the loop completes without a selection inform the user
+		if 	responseNumber == -1:
+				await message.channel.send('I\'m getting bored waiting for you, start over when you\'re ready.')
+				return
+	
+	#Identify which team is being requested by prompting the users with all returned results
+	async def prompt_team(self, message, searchTerm, teams):
+		#Check that more than one team was passed in
+		if len(teams) > 1:
+			#Build the string of teams to display
+			discordFormattedString = '>>> I found ' + str(len(teams)) + ' matches for \'' + searchTerm + '\' Enter the number for the team you want \n'
+			#Build the string to display the found players
+			for index in range(len(teams)):
+				#Add a newline for the next list item
+				if index < len(teams):
+					appendString = ' ' + str(index + 1) + ': ' + teams[index]['name'] + '\n'
+				else:
+					appendString = ' ' + str(index + 1) + ': ' + teams[index]['name']
+				discordFormattedString = discordFormattedString + appendString
+			await message.channel.send(discordFormattedString)
+			
+			messageTime = datetime.datetime.utcnow()
+			time.sleep(2)
+			teamSelectedIndex = 0
+			
+			#Wait 10 seconds to get an answer
+			for wait in range(1, 10):
+				if teamSelectedIndex != 0:
+					break;
+			
+				time.sleep(1)
+				#Get the last ten messages
+				messageList = await message.channel.history(limit=2).flatten()
+				
+				#if the name hasn't been selected yet
+				if teamSelectedIndex == 0:
+					#loop through the past 2 messages
+					for history in range (0, len(messageList)):
+						#The user who requested the list responded
+						if messageList[history].author == message.author:
+							#check if the message was sent after the list of names
+							if messageList[history].created_at > messageTime:
+								#The user responded with a number
+								if messageList[history].content.isdigit():
+									#The number is valid
+									if int(messageList[history].content) <= len(teams) and int(messageList[history].content) != 0:
+										teamSelectedIndex = int(messageList[history].content)
+										teamSelected = teams[teamSelectedIndex - 1]
+										break
+									else:
+										await message.channel.send('%s is not a valid number, start over' % str(messageList[history].content))
+										return
+								else:
+									await message.channel.send('%s is not a number, start over' % messageList[history].content)
+									return
+			#if the loop completes without a selection inform the user
+			if 	teamSelectedIndex == 0:
+				await message.channel.send('I\'m getting bored waiting for you, start over when you\'re ready.')
+				return
+			else:
+				return teamSelected
+	
+	async def playoffSeriesOver(self, series):
+		#Check for number of games
+		
+		'''
+		DEBUG: [{'id': 'S', 'description': 'Spring Training'}, {'id': 'R', 'description': 'Regular season'}, {'id': 'F', 'description': 'Wild Card Game'}, {'id': 'D', 'description': 'Division Series'}, {'id': 'L', 'description': 'League Championship Series'}, {'id': 'W', 'description': 'World Series'}, {'id': 'C', 'description': 'Championship'}, {'id': 'N', 'description': 'Nineteenth Century Series'}, {'id': 'P', 'description': 'Playoffs'}, {'id': 'A', 'description': 'All-Star game'}, {'id': 'I', 'description': 'Intrasquad'}, {'id': 'E', 'description': 'Exhibition'}]
+		'''
+		
+		#Wild card series
+		if series['series']['gameType'] == 'F':
+			if series['games'][0]['teams']['home']['leagueRecord']['wins'] > 0 or series['games'][0]['teams']['away']['leagueRecord']['wins'] > 0:
+				return True
+			else:
+				return False
+		#Division series
+		elif series['series']['gameType'] == 'D':
+			#Loop through all the games
+			for games in series['games']:
+				if games['teams']['home']['leagueRecord']['wins'] > 2 or games['teams']['away']['leagueRecord']['wins'] > 2:
+					return True
+			return False		
+		#Championship Series
+		elif series['series']['gameType'] == 'L':
+			#Loop through all the games
+			for games in series['games']:
+				if games['teams']['home']['leagueRecord']['wins'] > 4 or games['teams']['away']['leagueRecord']['wins'] > 4:
+					return True
+			return False
+		#World Series
+		elif series['series']['gameType'] == 'W':
+			#Loop through all the games
+			for games in series['games']:
+				if games['teams']['home']['leagueRecord']['wins'] > 4 or games['teams']['away']['leagueRecord']['wins'] > 4:
+					return True
+			return False
+		else:
+			print('DEBUG: ERROR playoffSeriesOver() failed with series type of %s' % series['series']['gameType'])
+			return False
+	
+	def get_Local_Time(self, dateTimeString):
+		gameTimeUTC = dateutil.parser.parse(dateTimeString)
+		# Tell the datetime object that it's in UTC time zone since 
+		# datetime objects are 'naive' by default
+		gameTimeUTC = gameTimeUTC.replace(tzinfo=dateutil.tz.tzutc())
+		#Convert to localtime
+		return gameTimeUTC.astimezone(dateutil.tz.tzlocal())
+	
+	async def sendGetRequest(self, url):
+		try:
+			#print('DEBUG: Sending HTTP request...')				
+			#build the headers
+			requestsHeaders = {'Content-Type': 'application/json'}
+			#Send the get request
+			response = requests.get(url, requestsHeaders)   
+			#print('DEBUG: Response HTTP Status Code: %s' % response.status_code)
+			#print('DEBUG: Response HTTP Response Body: %s' % response.content)
+			return response
+		except requests.exceptions.RequestException:
+			print('DEBUG: HTTP Request failed')
---
+++ puckbot.py
@@ -0,0 +1,97 @@
+import discord
+import json
+import datetime
+from datetime import timedelta
+import time
+import random
+import statsapi
+import re
+import dateutil.parser
+import requests
+import commonfunctions
+
+class HockeyBot(discord.Client):
+	commonFunctions = commonfunctions.CommonFunctions()
+		
+	
+	async def on_ready(self):
+		print('Logged on as', self.user)
+		await self.change_presence(activity=discord.Game(name='pond hockey'))
+	
+	async def on_message(self, message):
+		# don't respond to ourselves
+		if message.author == self.user:
+			return
+		else:
+			#Split the message at whitespace
+			messageArray = message.content.split()
+			if len(messageArray) > 0:
+				#Bot was called with enough arguments
+				if 'PUCKBOT' in messageArray[0].upper() and len(messageArray) > 1:
+					if 'SCHEDULE' in messageArray[1].upper():
+						hockeyEmbed = discord.Embed()
+						hockeyEmbed.title = 'Brrr its getting cold in here...'
+						hockeyEmbed.type = 'rich'
+						hockeyEmbed.color = discord.Color.dark_blue()
+						hockeyEmbed.set_image(url='https://i.imgur.com/yn7efui.png')
+						
+						#Get the NHL schedule
+						scheduleResponse = await self.commonFunctions.sendGetRequest('https://statsapi.web.nhl.com/api/v1/schedule')
+						
+						#Parse the JSON
+						scheduleJson = json.loads(scheduleResponse.text)			
+						
+						games = scheduleJson['dates'][0]['games']
+						
+						
+						for hockeyGame in games:
+							gameTimeLocal = self.commonFunctions.get_Local_Time(hockeyGame['gameDate'])
+							nameString = hockeyGame['teams']['away']['team']['name'] + ' vs ' + hockeyGame['teams']['home']['team']['name']
+							valueString = gameTimeLocal.strftime('%-I:%M%p') + ' EST' + ' @ ' + hockeyGame['venue']['name']
+							hockeyEmbed.add_field(name=nameString, value=valueString)
+						
+						contentString = 'Puckbot hipchecked ' + message.author.mention + ' into the boards! Savage! \n Anyway here are the games for ' + gameTimeLocal.strftime('%m/%d/%Y')
+						await message.channel.send(content=contentString, embed=hockeyEmbed)
+						
+					
+					#Display the help message
+					elif 'HELP' in messageArray[1].upper():
+						
+						helpEmbed = discord.Embed()
+						helpEmbed.title = 'Puckbot Help'
+						helpEmbed.type = 'rich'
+						helpEmbed.color = discord.Color.dark_blue()
+						helpEmbed.set_image(url='https://i.imgur.com/u7sswGk.jpg')
+						
+						await message.channel.send(embed=helpEmbed)
+					else:
+						await message.channel.send('Sorry I don\'t understand, try saying \'Puckbot HELP\' for a list of available commands.')	
+
+				else:
+					await message.channel.send('Sorry I don\'t understand, try saying \'Puckbot HELP\' for a list of available commands.')
+
+def ReadTokenFile(filename):
+	#Read the token from a file
+	try:
+		key_file = open(filename, "r")
+		token = key_file.read()
+		key_file.close()
+		return token
+	except:
+		print("Error loading %s" % filename)
+		sys.exit(1)
+	else:
+		print("No %s file found!" % filename)
+
+def main():
+	client = HockeyBot()
+	token = ReadTokenFile('auth')
+	
+	client.run(token)
+	
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		print('Stopping Puckbot')
+		pass
